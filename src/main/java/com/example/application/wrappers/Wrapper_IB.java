fa87527f1a21f07cca42505b06a9bc6a24a1ee60
ipiqcer <ipiqcer@inf.upv.es>
2023-01-22 20:11:26 +0100

    Comentarios

--- src/main/java/com/example/application/wrappers/Wrapper_IB.java
+++ src/main/java/com/example/application/wrappers/Wrapper_IB.java
@@ -11,6 +11,8 @@ import com.example.application.util.Lector;
 public class Wrapper_IB {
     private String xml;
 
+    // El objeto tiene un atributo xml donde se guardará el contenido del archivo que leemos mediante
+    // el método leerFicheroDeTexto, al cual le pasamos como argumento la ruta al fichero.
     public Wrapper_IB(String path) {
         try {
             this.xml = Lector.leerFicheroDeTexto(path);
@@ -18,7 +20,8 @@ public class Wrapper_IB {
             e.printStackTrace();
         }
     }
-
+    // Este método se encarga de coger el contenido del archivo xml para aplicarle la conversión al modelo
+    // de datos JSON que vamos a usar en la integración.
     public String getJSON() {
         JSONObject obj = (JSONObject) XML.toJSONObject(this.xml);
         JSONObject response = (JSONObject) obj.get("response");
